cd1903d313270a0a772471a1c3ec2eaf1961509e
Igor Buzykin <lexemz@gmail.com>
2021-10-06 16:25:03 +0300

    Created StorageManager

---
+++ CoreDataDemo/StorageManager.swift
@@ -0,0 +1,38 @@
+//
+//  StorageManager.swift
+//  CoreDataDemo
+//
+//  Created by Igor on 06.10.2021.
+//
+
+import CoreData
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    init() {}
+    
+    // MARK: - Core Data stack
+    var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CoreDataDemo")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
